89fc78dccc0a1a0afcf6ea2201cedf19585f9995
crissvargas <cristhina.vargas@tecsup.edu.pe>
2022-09-20 23:57:21 -0500

    Laboratorio 05

---
+++ Semana05/Semana05/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Business;
+using Entity;
+
+namespace Semana05
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
